72a2c664ca20ef3329bb5dee95570d40afcf89c2
Shayla White <shayladwhite1@gmail.com>
2020-03-21 21:03:59 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!! I BULIT THIS"
+    "Welcome to your app!!!! I BLUIT THIS"
   end
 
 end
